e3c9ec05c47a02d28b2b26e319572e28da16253e
AbdullahProg111 <saqlainabdullah76@gmail.com>
2024-07-22 17:39:18 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed('#element', {
+    strings: ['Full Stack Developer', 'Cloud Developer', 'SQL Developer', 'Software Engineer', 'Software Developer', 'Backend Developer'],
+    typeSpeed: 50,
+  });
+
+//          scroll smoothness 
+
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('#main'),
+  smooth: true
+});
